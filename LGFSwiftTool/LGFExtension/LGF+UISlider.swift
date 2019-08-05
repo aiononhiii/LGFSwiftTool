bfdcbdd3d22f0da6cc20c4f97dff8c4fc6b31cde
aiononhi <452354033@qq.com>
2019-08-05 11:27:44 +0800

    0.1.0

--- LGFSwiftTool/LGFExtension/LGF+UISlider.swift
+++ LGFSwiftTool/LGFExtension/LGF+UISlider.swift
@@ -10,12 +10,14 @@
 import UIKit
 
 extension UISlider {
+    
     // MARK: - 动画 setValue
     public func lgf_SetAnimateValue(_ value: Float, duration: Double) {
         UIView.animate(withDuration: duration, animations: { () -> Void in
             self.setValue(value, animated: true)
         }, completion: nil)
     }
+    
 }
 
 #endif // canImport(UIKit)
