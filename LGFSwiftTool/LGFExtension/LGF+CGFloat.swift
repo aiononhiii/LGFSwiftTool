05141c8a56e0ba297f0fe19ae0d970c7fa3cfc86
aiononhi <452354033@qq.com>
2019-08-20 13:27:37 +0800

    0.2.7

---
+++ LGFSwiftTool/LGFExtension/LGF+CGFloat.swift
@@ -0,0 +1,26 @@
+//
+//  LGF+CGFloat.swift
+//  LGFSwiftTool
+//
+//  Created by apple on 2019/8/20.
+//  Copyright © 2019 来国锋. All rights reserved.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+public extension CGFloat {
+    
+    // MARK: - 获取相对于屏幕宽度
+    func lgf_ScreenW() -> CGFloat {
+        return self *  UIDevice.lgf_ScreenW / 375.0
+    }
+    
+    // MARK: - 获取相对于屏幕高度
+    func lgf_ScreenH() -> CGFloat {
+        return self *  UIDevice.lgf_ScreenH / 667.0
+    }
+    
+}
+
+#endif // canImport(UIKit)
